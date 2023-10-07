5ad7edf3e4b07d18bb8a36a8555a5fb124607127
nicolecraven <craventhegreat@gmail.com>
2023-10-07 13:34:41 -0400

    finished lab

---
+++ index.js
@@ -0,0 +1,29 @@
+//function divide() {
+   // return 'divide';
+  //}
+
+  //const divide = function() {
+    //return 'divide';
+  //}  
+
+  const divide = (parameter1, parameter2) => parameter1 / parameter2;
+  divide (2000,100);
+
+  //const square = function(){
+    //return 'square';
+  //}
+
+  const square = x => x * x;
+
+  //const add = function (){
+    //return 'add';
+  //}
+
+  const add = (parameter1, parameter2) => parameter1 + parameter2;
+  add(2,3);
+
+
+
+
+
+  
